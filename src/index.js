81561264fdba5fe9627dcdad6cd422aacc7f2c47
elsehow <yes@cosmopol.is>
2016-10-06 19:11:50 -0700

    Added openbci working example

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-module.exports = 'hello'
+var EventEmitter = require('events').EventEmitter
+var OpenBCIBoard = require('openbci').OpenBCIBoard
+// opts is
+//   { debug (boolean), simulate (boolean) }
+// returns an emitter
+// emitter will emit
+//   - 'sample' (with an array of volts)
+//   - 'error'
+function openBCI (opts) {
+  var emitter = new EventEmitter()
+  var ourBoard = new OpenBCIBoard({
+    simulate: opts.simulate,
+  })
+  ourBoard
+    .autoFindOpenBCIBoard()
+    .then(function(portName) {
+      if (!portName && !opts.simulate) {
+        emitter.emit('error', 'No port found!')
+        return
+      }
+      if (opts.debug) console.log('found', portName)
+      ourBoard.connect(portName)
+      ourBoard.on('ready',function() {
+        if (opts.debug) console.log('connected, ready')
+        ourBoard.streamStart()
+        ourBoard.on('sample',function(sample) {
+          var readings = sample.channelData.map(x => x.toFixed(8))
+          if (opts.debug) console.log('volts (index is channel)', readings)
+          emitter.emit('sample', readings)
+        })
+      })
+    }).catch(err => emitter.emit("error", err))
+
+      return emitter
+}
+
+module.exports = openBCI
--- test/index.js
+++ test/index.js
@@ -1,3 +1,7 @@
-var mod = require('..')
-
-console.log('imported', mod)
+var openbci = require('..')
+var eeg = openbci({ 
+  debug: true,
+  simulate: false,
+})
+eeg.on("error", err => console.log('ERR!', err))
+eeg.on("reading", reading => console.log(reading))
